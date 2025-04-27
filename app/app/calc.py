152e6edb84b6f417ee96619fa4879a47dd9eb487
hussamElmaghraby <hussamelmaghrabi@gmail.com>
2025-04-27 03:32:53 +0300

    connect to db

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+
+
+
+
+def add(x , y):
+    """Add x and y  return result."""
+    return x + y
+
+
+def substract(x , y):
+    """Sub x from y"""
+    return x - y
+
